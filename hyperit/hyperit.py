8eacb5e03c262ecb1ca03661d52ccd3842e9eee0
Edoardo Chidichimo <ec750@cam.ac.uk>
2024-04-21 19:02:07 +0100

    correcting utils import

--- hyperit/hyperit.py
+++ hyperit/hyperit.py
@@ -10,7 +10,7 @@ from jpype import isJVMStarted, startJVM, getDefaultJVMPath
 from phyid.calculate import calc_PhiID
 from phyid.utils import PhiID_atoms_abbr
 
-from utils import setup_JArray, bandpass_filter_data, convert_names_to_indices, convert_indices_to_names, set_estimator, text_positions
+from .utils import setup_JArray, bandpass_filter_data, convert_names_to_indices, convert_indices_to_names, set_estimator, text_positions
 
 from enum import Enum
 
