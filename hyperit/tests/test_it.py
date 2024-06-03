a66bed6e7cc46d9d762683f4e5587a18990c1093
Edoardo Chidichimo <ec750@cam.ac.uk>
2024-06-03 17:21:17 +0100

    update tests to reflect major update

--- hyperit/tests/test_it.py
+++ hyperit/tests/test_it.py
@@ -16,21 +16,12 @@ class TestHyperIT(unittest.TestCase):
         self.channels = [['C1', 'C2', 'C3'], ['C1', 'C2', 'C3']]
         self.data1 = np.random.rand(10, 3, 600)  # 10 epochs, 3 channels, 100 samples
         self.data2 = np.random.rand(10, 3, 600)
-        self.freq_bands = {'alpha': (8, 12)}
-        self.sfreq = 256  # Hz
-        self.jarLocation = os.path.join(os.path.dirname(os.path.dirname(__file__)))
-        self.hyperit_instance = HyperIT(self.data1, self.data2, self.channels, self.sfreq, self.freq_bands)
-
-    @patch('hyperit.HyperIT.setup_JVM')
-    def test_initialization(self, mock_setup_jvm):
-        """Test object initialization and JVM setup call."""
-        mock_setup_jvm.assert_not_called()
-        self.assertEqual(self.hyperit_instance._sfreq, self.sfreq)
+        self.hyperit_instance = HyperIT(self.data1, self.data2, self.channels)
 
     def test_check_data_valid(self):
         """Test the data validation logic with correct input."""
         try:
-            HyperIT(self.data1, self.data2, self.channels, self.sfreq, self.freq_bands)
+            HyperIT(self.data1, self.data2, self.channels)
         except Exception as e:
             self.fail(f"Initialization failed with correct data: {e}")
 
@@ -38,7 +29,7 @@ class TestHyperIT(unittest.TestCase):
         """Test the data validation logic with incorrect input shapes."""
         data_wrong = np.random.rand(2,2,2,100)
         with self.assertRaises(ValueError):
-            HyperIT(data_wrong, data_wrong, self.channels, self.sfreq, self.freq_bands)
+            HyperIT(data_wrong, data_wrong, self.channels)
 
     @patch.object(HyperIT, 'roi', new_callable=PropertyMock)
     def test_roi_setting(self, mock_roi):
@@ -55,13 +46,13 @@ class TestHyperIT(unittest.TestCase):
 
     def test_mi_computation(self):
         """Test Mutual Information computation."""
-        newitmi = HyperIT(self.data1, self.data2, self.channels, self.sfreq, self.freq_bands)
+        newitmi = HyperIT(self.data1, self.data2, self.channels)
         newitmi.compute_mi('histogram', include_intra=True)
         self.assertIsNotNone(newitmi._it_matrix)
 
     def test_te_computation(self):
         """Test Transfer Entropy computation setup."""
-        newitte = HyperIT(self.data1, self.data2, self.channels, self.sfreq, self.freq_bands)
+        newitte = HyperIT(self.data1, self.data2, self.channels)
         newitte.compute_te('gaussian', include_intra=False)
         self.assertIsNotNone(newitte._it_matrix)
 
